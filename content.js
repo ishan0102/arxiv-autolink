a99fa17bd33a2b2406d547a3d5d5f9971399042c
Ishan Shah <ishan0102@utexas.edu>
2023-06-08 13:21:31 -0700

    Remove duplicate links

--- content.js
+++ content.js
@@ -4,19 +4,24 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.message === "getArxivLinks") {
         // Get all <a> elements on the page
         const links = document.getElementsByTagName("a");
-        let arxivLinks = [];
+        let arxivLinks = {};
 
         for (let link of links) {
             if (arxivLinkPattern.test(link.href)) {
                 let modifiedLink = link.href.replace('/pdf/', '/abs/');
                 modifiedLink = modifiedLink.replace('.pdf', '');
-                arxivLinks.push({
+
+                // Add the link object to the arxivLinks object
+                // If the URL is already in the object, it will simply overwrite the existing entry
+                arxivLinks[modifiedLink] = {
                     link: modifiedLink,
                     position: link.getBoundingClientRect().top + window.pageYOffset,
-                });
+                };
             }
         }
 
+        // Convert the Object to an Array
+        arxivLinks = Object.values(arxivLinks);
         sendResponse({ arxivLinks: arxivLinks });
     } else if (request.message === "scrollToLink") {
         window.scrollTo({ top: request.position - 50, behavior: 'smooth' });
